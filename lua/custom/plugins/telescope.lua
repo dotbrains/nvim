23bbb2aec0528d077b849f67adaada192ebfee9b
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-27 16:21:09 -0500

    added nvim-telescope/telescope-ui-select.nvim

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,55 +1,71 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    cmd = "Telescope",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
+    {
+        "nvim-telescope/telescope.nvim",
+        cmd = "Telescope",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+
+            telescope.setup({
+                pickers = {
+                    colorscheme = {
+                        enable_preview = true,
+                    },
+
+                    find_files = {
+                        file_ignore_patterns = { ".git%p" },
+                    },
+                },
+                defaults = {
+                    hidden = true,
+                    selection_caret = "  ",
+                    entry_prefix = "  ",
+
+                    sorting_strategy = "ascending",
+                    layout_strategy = "horizontal",
+
+                    layout_config = {
+                        horizontal = {
+                            prompt_position = "top",
+                            preview_width = 0.55,
+                            results_width = 0.8,
+                        },
+                        vertical = {
+                            mirror = false,
+                        },
+                        width = 0.87,
+                        height = 0.80,
+                        preview_cutoff = 120,
+                    },
+                    path_display = { "truncate " },
+                    mappings = {
+                        i = {
+                            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                            ["<C-j>"] = actions.move_selection_next, -- move to next result
+                            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        },
+                    },
+                },
+            })
+        end
     },
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
-
-        telescope.setup({
-            pickers = {
-                colorscheme = {
-                    enable_preview = true,
-                },
-
-                find_files = {
-                    file_ignore_patterns = { ".git%p" },
-                },
-            },
-            defaults = {
-                hidden = true,
-                prompt_prefix = " >  ",
-                selection_caret = "  ",
-                entry_prefix = "  ",
-
-                sorting_strategy = "ascending",
-                layout_strategy = "horizontal",
-
-                layout_config = {
-                    horizontal = {
-                        prompt_position = "top",
-                        preview_width = 0.55,
-                        results_width = 0.8,
-                    },
-                    vertical = {
-                        mirror = false,
-                    },
-                    width = 0.87,
-                    height = 0.80,
-                    preview_cutoff = 120,
-                },
-                path_display = { "truncate " },
-                mappings = {
-                    i = {
-                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-                        ["<C-j>"] = actions.move_selection_next, -- move to next result
-                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-                    },
-                },
-            },
-        })
-    end,
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown{}
+                    }
+                }
+            }
+            -- To get ui-select loaded and working with telescope, you need to call
+            -- load_extension, somewhere after setup function:
+            require("telescope").load_extension("ui-select")
+        end
+    }
 }
