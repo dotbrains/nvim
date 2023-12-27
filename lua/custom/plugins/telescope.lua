5cace058c8a887df48e94cf64be14bf1f3d50b47
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-27 16:26:19 -0500

    updated telescope.lua

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -20,6 +20,15 @@ return {
                         file_ignore_patterns = { ".git%p" },
                     },
                 },
+                extensions = {
+                    fzf = {
+                        fuzzy = true,                   -- false will only do exact matching
+                        override_generic_sorter = true, -- override the generic sorter
+                        override_file_sorter = true,    -- override the file sorter
+                        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                                                        -- the default case_mode is "smart_case"
+                    }
+                }
                 defaults = {
                     hidden = true,
                     selection_caret = "  ",
@@ -51,6 +60,10 @@ return {
                     },
                 },
             })
+
+            -- To get fzf loaded and working with telescope, you need to call
+            -- load_extension, somewhere after setup function:
+            require('telescope').load_extension('fzf')
         end
     },
     {
@@ -67,5 +80,9 @@ return {
             -- load_extension, somewhere after setup function:
             require("telescope").load_extension("ui-select")
         end
+    },
+    { 
+        'nvim-telescope/telescope-fzf-native.nvim', 
+        build = 'make' -- Requires make, gcc, and clang to be installed
     }
 }
