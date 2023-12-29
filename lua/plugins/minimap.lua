817dc2ab6bd1541859567cd3b572fbdb1bdfdaac
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-29 14:11:40 -0500

    fixed minimap plugin

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -2,11 +2,16 @@ local config = function()
     local minimap = require("mini.map")
 
     minimap.setup()
+
+    -- Automatically open the minimap when opening vim
+    vim.cmd("autocmd VimEnter * MiniMap.open()")
+
 end
 
 return {
     "echasnovski/mini.map", 
     version = '*',
     lazy = false,
+    cmd = 'MiniMap',
     config = config
 }
