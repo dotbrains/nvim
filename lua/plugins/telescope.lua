ce6679cc6eb6bef3f4caef2fbca988e129dc7365
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-29 15:03:53 -0500

    fixed telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,20 +11,6 @@ local config = function()
 					["<C-j>"] = actions.move_selection_next,
 					["<C-k>"] = actions.move_selection_previous,
 				},
-				layout_strategy = "horizontal",
-				layout_config = {
-                    horizontal = {
-                        prompt_position = "bottom",
-                        preview_width = 0.55,
-                        results_width = 0.8,
-                    },
-                    vertical = {
-                        mirror = false,
-                    },
-                    width = 0.87,
-                    height = 0.80,
-                    preview_cutoff = 120,
-                },
 			},
 		},
 		pickers = {
