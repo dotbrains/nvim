eb35bcea80bcc79b450754d13e1558d91685e3c9
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-29 13:44:38 -0500

    fixed minimap keybindings

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -14,6 +14,6 @@ return {
     "wfxr/minimap.vim",
     config = config,
     keys = {
-        mapvimkey("n", "<leader>mm", ":MinimapToggle", "MinimapToggle"),
+        mapvimkey("<leader>mm", ":MinimapToggle", "MinimapToggle"),
     }
 }
