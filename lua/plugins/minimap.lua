0ca80dbdec355a9b0da2967d16a5f6f84d6a6871
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-29 13:50:42 -0500

    fixed minimap plugin

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -3,11 +3,13 @@ local mapvimkey = require("util.keymapper").mapvimkey
 local config = function()
     local minimap = require("minimap")
 
-    minimap.minimap_auto_start = 1 -- start minimap when opening file
-    minimap.minimap_auto_start_win_enter = 1 -- start minimap when entering window
-    minimap.minimap_git_colors = 1 -- enable git colors for minimap
-    minimap.minimap_highlight_range = 1 -- highlight range under cursor
-    minimap.minimap_highlight_search = 1 -- highlight search results
+    minimap.setup({
+        minimap_auto_start = 1, -- start minimap when opening file
+        minimap_auto_start_win_enter = 1, -- start minimap when entering window
+        minimap_git_colors = 1, -- enable git colors for minimap
+        minimap_highlight_range = 1, -- highlight range under cursor
+        minimap_highlight_search = 1, -- highlight search results
+    })
 end
 
 return {
