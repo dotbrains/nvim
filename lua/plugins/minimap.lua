66e7fabd74b9b78591c351fc2c1881811b7ee216
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-29 14:42:24 -0500

    fixed minimap

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,22 +1,17 @@
 local mapvimkey = require("util.keymapper").mapvimkey
 
-local config = function()
+local init = function()
     vim.g.minimap_auto_start = 1
     vim.g.minimap_auto_start_win_enter = 1
     
     vim.g.minimap_highlight_range = 1
     vim.g.minimap_highlight_search = 1
-
-    -- Start minimap when vim starts
-    vim.api.nvim_exec([[
-      autocmd VimEnter * Minimap
-    ]], false)
 end
 
 return {
     "wfxr/minimap.vim", 
     lazy = false,
-    config = config,
+    init = init,
     keys = {
         mapvimkey("<leader>mm", ":MinimapToggle", "MinimapToggle"),
     }
