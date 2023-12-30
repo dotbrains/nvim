f9ad7b19a331cb1afda3a68392e418ad70539cde
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-30 07:00:11 -0500

    fixed config for lsp_zero

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,5 +1,15 @@
 local mapvimkey = require("util.keymapper").mapvimkey
 
+local config = function()
+    local lsp_zero = require('lsp-zero')
+
+    lsp_zero.on_attach(function(client, bufnr)
+        -- see :help lsp-zero-keybindings
+        -- to learn the available actions
+        lsp_zero.default_keymaps({buffer = bufnr})
+    end)
+end
+
 return {
     'VonHeikemen/lsp-zero.nvim', 
     branch = 'v3.x',
