b8e82169b46e33d0c627f79da5e90cb084deeddf
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-30 07:08:57 -0500

    rm lsp-zero

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -14,30 +14,11 @@ local opts = {
 	automatic_installation = true,
 }
 
-local config = function()
-	local mason_lspconfig = require("mason-lspconfig")
-	local lsp_zero = require("lsp-zero")
-	local lspconfig = require("lspconfig")
-
-	mason_lspconfig.setup({
-		handlers = {
-			lsp_zero.default_setup,
-			lua_ls = function()
-				local lua_opts = lsp_zero.nvim_lua_ls()
-				lspconfig.lua_ls.setup(lua_opts)
-			end,
-		}
-	})
-end
-
 return {
 	"williamboman/mason-lspconfig.nvim",
 	opts = opts,
-	config = config,
 	event = "BufReadPre",
 	dependencies = {
 		"williamboman/mason.nvim",
-		"VonHeikemen/lsp-zero.nvim",
-		"neovim/nvim-lspconfig",
 	},
 }
