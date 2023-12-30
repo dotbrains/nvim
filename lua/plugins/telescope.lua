f3d336e8d34891117c56271a268e2c6ac22ac74f
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-30 07:12:42 -0500

    rm a few plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -48,7 +48,7 @@ local config = function()
 	-- To get extensions loaded and working with telescope, you need to call
 	-- load_extension, somewhere after setup function:
 	
-	local extensions = {'fzf', 'harpoon', 'git_worktree', 'noice', 'aerial'}
+	local extensions = {'fzf', 'harpoon', 'git_worktree', 'noice'}
 
 	for _, extension in ipairs(extensions) do
 		telescope.load_extension(extension)
@@ -71,7 +71,6 @@ return {
 			"ThePrimeagen/harpoon",
 			"ThePrimeagen/git-worktree.nvim",
 			"folke/noice.nvim",
-			"stevearc/aerial.nvim",
 		},
 		config = config,
 		keys = {
