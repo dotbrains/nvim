469c4bd1d8c54288945cdb760087744c89fa2b11
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-30 22:00:20 -0500

    more comments

--- lua/plugins/utilities/nvim-treesitter.lua
+++ lua/plugins/utilities/nvim-treesitter.lua
@@ -1,3 +1,9 @@
+-- Nvim Treesitter configurations and abstraction layer.
+-- The goal of nvim-treesitter is both to provide a simple and easy way to use the 
+-- interface for tree-sitter in Neovim and to provide some basic functionality such 
+-- as highlighting based on it.
+-- see: https://github.com/nvim-treesitter/nvim-treesitter
+
 local config = function()
 	require("nvim-treesitter.configs").setup({
 		build = ":TSUpdate",
