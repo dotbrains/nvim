e8c378c444a86322eeda6929dc34e1cc0dda7781
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-30 21:51:57 -0500

    refactored keymappings for true-zen

--- lua/plugins/utilities/true-zen.lua
+++ lua/plugins/utilities/true-zen.lua
@@ -1,16 +1,21 @@
 -- Clean and elegant distraction-free writing for NeoVim.
 -- see: https://github.com/pocco81/true-zen.nvim
 
+local mapvimkey = require("util.keymapper").mapvimkey
+
 return {
 	"pocco81/true-zen.nvim",
 	lazy = false,
 	config = function()
 		local api = vim.api
 
-		api.nvim_set_keymap("n", "<leader>zn", ":TZNarrow<CR>", {})
-		api.nvim_set_keymap("v", "<leader>zn", ":'<,'>TZNarrow<CR>", {})
-		api.nvim_set_keymap("n", "<leader>zf", ":TZFocus<CR>", {})
-		api.nvim_set_keymap("n", "<leader>zm", ":TZMinimalist<CR>", {})
-		api.nvim_set_keymap("n", "<leader>za", ":TZAtaraxis<CR>", {})
+		-- normal mode mappings
+		mapvimkey("<leader>zn", "TZNarrow", "Narrow")
+		mapvimkey("<leader>zf", "TZFocus", "Focus")
+		mapvimkey("<leader>zm", "TZMinimalist", "Minimalist")
+		mapvimkey("<leader>za", "TZAtaraxis", "Ataraxis")
+
+		-- visual mode mappings
+		mapvimkey("<leader>zn", ":'<,'>TZNarrow", "Narrow", "v")
 	end
 }
