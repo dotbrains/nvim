963ec3b679db16d4258ecf8c37d697f66c2fa5ab
Nicholas Adamou <nicholasadamouemail@gmail.com>
2023-12-31 15:26:20 -0500

    updated get_plugin_paths

--- lua/util/plugins.lua
+++ lua/util/plugins.lua
@@ -1,20 +1,21 @@
 -- get all subdirectories and Lua files from given directory
-function get_plugin_paths(directory)
+local function get_plugin_paths(directory)
     local plugin_paths = {}
+    local scan
 
-    -- Use Neovim's vim.loop.fs_scandir to iterate over directory contents
-    local scan = vim.loop.fs_scandir(directory)
-    if scan then
-        while true do
-            local filename, type = vim.loop.fs_scandir_next(scan)
-            if not filename then break end
-
-            if type == "directory" or string.match(filename, "%.lua$") then
-                local path = directory .. '/' .. filename
-                table.insert(plugin_paths, path)
+    -- Recursive function to scan directories
+    scan = function(dir)
+        local p = io.popen('find "'..dir..'" -type f')   -- Open directory look for files, save data to p. 
+        for file in p:lines() do                         -- Loop through all files
+            if file:match("%.lua$") then                 -- Filter out only Lua files
+                table.insert(plugin_paths, file)
             end
         end
+        p:close()
     end
+
+    -- Start scanning from the base directory
+    scan(directory)
 
     return plugin_paths
 end
