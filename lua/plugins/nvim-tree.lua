e7439e8965372212c3cdd4a652af36b617d5465a
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-09-30 06:33:28 -0400

    added additional configs to nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,6 +4,7 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	lazy = false,
+	requires = { "nvim-tree/nvim-web-devicons" }, -- Optional: for file icons
 	config = function()
 		require("nvim-tree").setup({
 			filters = {
@@ -11,7 +12,24 @@ return {
 			},
 			view = {
 				adaptive_size = true,
+				width = 30, -- Set the width of the tree view
+				side = "left", -- Position of the tree view
+				mappings = {
+					list = {
+						{ key = {"<CR>", "o"}, action = "edit" }, -- Open file
+						{ key = "O", action = "edit_no_picker" }, -- Open file without picker
+						{ key = "e", action = "edit_in_place" }, -- Edit in place
+						{ key = "u", action = "dir_up" }, -- Go up a directory
+						{ key = "s", action = "split" }, -- Split window
+						{ key = "v", action = "vsplit" }, -- Vertical split
+					},
+				},
 			},
+			git = {
+				enable = true, -- Enable git integration
+				ignore = false, -- Show ignored files
+			},
+			-- Other options can be added here
 		})
 	end,
 }
