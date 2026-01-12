93af86bdeb8b0f5ef5aabcf01ac6c8f201c71047
Nicholas Adamou <nicholasadamouemail@gmail.com>
2026-01-12 04:48:44 -0500

    style: fix stylua formatting in leap-nvim.lua

--- lua/plugins/editor/leap-nvim.lua
+++ lua/plugins/editor/leap-nvim.lua
@@ -10,10 +10,10 @@ return {
   "ggandor/leap.nvim",
   event = "BufEnter",
   config = function()
-    local leap = require("leap")
+    local leap = require "leap"
     -- Set up keymaps as recommended in :help leap-mappings
-    vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
-    vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
-    vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
+    vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+    vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+    vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
   end,
 }
